096e5a1d302cc7900d185437d9715c28eaa94fae
pallavibhadre <109409156+pallavibhadre@users.noreply.github.com>
2022-07-17 15:11:30 +0530

    Create BasicMicrosarviceProject.java

---
+++ BasicMicrosarviceProject.java
@@ -0,0 +1,15 @@
+package com.example.basicMicrosarviceProject;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicMicrosarviceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicMicrosarviceProjectApplication.class, args);
+		System.out.println("App started");
+	}
+
+}
